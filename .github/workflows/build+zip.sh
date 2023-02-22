cdd484589d0b1f4089de7115192f6d5da5a2e76b
ygguser <there_is@no.email>
2023-02-23 02:53:16 +0300

    Windows binaries will no longer be packaged using UPX

--- .github/workflows/build+zip.sh
+++ .github/workflows/build+zip.sh
@@ -7,11 +7,10 @@ cross +nightly build --release --target="$1"
 
 if [[ "$1" == *"windows"* ]]; then
     BINNAME="target/$1/release/peers_updater.exe"
-    zip -9 -j "target/$1/release/no_upx_$1.zip" "$BINNAME"
 else
     BINNAME="target/$1/release/peers_updater"
     chmod og+x "$BINNAME"
+    upx --ultra-brute "$BINNAME"
 fi
 
-upx --ultra-brute "$BINNAME"
 zip -9 -j "target/$1/release/$1.zip" "$BINNAME"
