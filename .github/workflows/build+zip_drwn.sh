cdd189ffd4b80d129b9a496cb50c50bc81308ecf
ygguser <86843390+ygguser@users.noreply.github.com>
2023-10-29 13:11:24 +0300

    Create build+zip_drwn.sh

---
+++ .github/workflows/build+zip_drwn.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DIR=$(git rev-parse --show-toplevel)
+cd "$DIR"
+
+#MACOS_TARGET="x86_64-apple-darwin"
+MACOS_TARGET="$1"
+
+echo "Building target for platform ${MACOS_TARGET}"
+echo
+
+# Add osxcross toolchain to path
+export PATH="$(pwd)/osxcross/target/bin:$PATH"
+
+# Make libz-sys (git2-rs -> libgit2-sys -> libz-sys) build as a statically linked lib
+# This prevents the host zlib from being linked
+export LIBZ_SYS_STATIC=1
+
+# Use Clang for C/C++ builds
+export CC=o64-clang
+export CXX=o64-clang++
+
+#cross +nightly build --release --target="$1"
+cargo build --release --target "${MACOS_TARGET}"
+
+BINNAME="target/$1/release/peers_updater"
+chmod og+x "$BINNAME"
+upx --ultra-brute "$BINNAME"
+
+zip -9 -j "target/$1/release/$1.zip" "$BINNAME"
