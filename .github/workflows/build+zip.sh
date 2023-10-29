8adb69b206c334e2c031c8196f69a108ec24801c
ygguser <there_is@no.email>
2023-10-29 14:03:16 +0300

    "combing"

--- .github/workflows/build+zip.sh
+++ .github/workflows/build+zip.sh
@@ -3,11 +3,7 @@
 DIR=$(git rev-parse --show-toplevel)
 cd "$DIR"
 
-#if [[ "$1" == *"darwin"* ]]; then
-    #cargo build --release --target $1
-#else
-    cross +nightly build --release --target="$1"
-#fi
+cross +nightly build --release --target="$1"
 
 if [[ "$1" == *"windows"* ]]; then
     BINNAME="target/$1/release/peers_updater.exe"
