a65ddd6c9202f1a18a5372f8253cc814ca79e108
ygguser <86843390+ygguser@users.noreply.github.com>
2023-10-29 13:18:46 +0300

    Update build+zip_drwn.sh

--- .github/workflows/build+zip_drwn.sh
+++ .github/workflows/build+zip_drwn.sh
@@ -3,6 +3,12 @@
 DIR=$(git rev-parse --show-toplevel)
 cd "$DIR"
 
+git clone https://github.com/tpoechtrager/osxcross
+cd osxcross
+wget -nc https://s3.dockerproject.org/darwin/v2/MacOSX10.10.sdk.tar.xz
+mv MacOSX10.10.sdk.tar.xz tarballs/
+UNATTENDED=yes OSX_VERSION_MIN=10.7 ./build.sh
+
 #MACOS_TARGET="x86_64-apple-darwin"
 MACOS_TARGET="$1"
 
