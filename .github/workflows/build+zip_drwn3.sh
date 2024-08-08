4c5fd6f38eb00a602872e9753077445d3bf9de2e
ygguser <86843390+ygguser@users.noreply.github.com>
2024-08-08 17:43:17 +0300

    Update build+zip_drwn3.sh

--- .github/workflows/build+zip_drwn3.sh
+++ .github/workflows/build+zip_drwn3.sh
@@ -2,8 +2,8 @@
 
 rustup target add x86_64-apple-darwin
 
-cargo build --target x86_64-apple-darwin
+cargo build --relese --target x86_64-apple-darwin
 
-BINNAME="target/$1/release/peers_updater"
+BINNAME="target/release/peers_updater"
 chmod og+x "$BINNAME"
-zip -9 -j "target/$1/release/$1.zip" "$BINNAME"
+zip -9 -j "target/release/x86_64-apple-darwin.zip" "$BINNAME"
