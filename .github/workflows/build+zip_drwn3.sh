066d635081fe57d87108798f27030830f40df695
YggUser <there_is@no.email>
2024-08-08 17:35:26 +0300

    build+zip_drwn3.sh

---
+++ .github/workflows/build+zip_drwn3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rustup target add x86_64-apple-darwin
+
+cargo build --target x86_64-apple-darwin
+
+BINNAME="target/$1/release/peers_updater"
+chmod og+x "$BINNAME"
+zip -9 -j "target/$1/release/$1.zip" "$BINNAME"
