abacad2b9647b2b921ba2734bfe9c279f89ca83d
ygguser <there_is@no.email>
2024-08-08 20:05:25 +0300

    delete unnecessary

--- .github/workflows/build+zip_drwn3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rustup target add x86_64-apple-darwin
-
-cargo build --release --target x86_64-apple-darwin
-
-BINNAME="build/release/peers_updater"
-chmod og+x "$BINNAME"
-zip -9 -j "build/release/x86_64-apple-darwin.zip" "$BINNAME"
